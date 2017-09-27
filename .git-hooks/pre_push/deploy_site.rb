1d6f751e3efdf9419829febcfd798bf449447ed7
Dana Merrick <dana@snapdocs.com>
2017-09-27 16:02:06 -0700

    Skip builds on non-master branches.

--- .git-hooks/pre_push/deploy_site.rb
+++ .git-hooks/pre_push/deploy_site.rb
@@ -1,6 +1,10 @@
 module Overcommit::Hook::PrePush
   class DeploySite < Base
     def run
+      if Overcommit::GitRepo.current_branch != 'master'
+        return [:pass, 'Skipping non-master branch']
+      end
+
       system(command, flags.join(' '), out: $stdout, err: :out)
       result = $?
       return :pass if result.success?
