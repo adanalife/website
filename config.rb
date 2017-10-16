f9111cbfec193573e682d8ec89a16ca55e034a45
Dana Merrick <dana@snapdocs.com>
2017-10-15 20:39:37 -0700

    Removing stuff I don't need.

--- config.rb
+++ config.rb
@@ -39,12 +39,6 @@ activate :syntax
 # Shrink images during build
 activate :imageoptim
 
-# Automatic image dimensions on image_tag helper
-#activate :automatic_image_sizes
-
-# Integrate Dotenv
-#activate :dotenv
-
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
